f49acd563eea2eee007139c282a398ea945dd113
Moon <101085046+MOON-CODES@users.noreply.github.com>
2024-12-01 15:50:09 +0530

    Update __init__.py

--- AnonXMusic/platforms/__init__.py
+++ AnonXMusic/platforms/__init__.py
@@ -1,18 +1,7 @@
-#
-# Copyright (C) 2024 by TheTeamVivek@Github, < https://github.com/TheTeamVivek >.
-#
-# This file is part of < https://github.com/TheTeamVivek/YukkiMusic > project,
-# and is released under the MIT License.
-# Please see < https://github.com/TheTeamVivek/YukkiMusic/blob/master/LICENSE >
-#
-# All rights reserved.
-#
-
-from .Apple import Apple
-from .Carbon import Carbon
-from .JioSavan import Saavn
-from .Resso import Resso
-from .Soundcloud import SoundCloud
-from .Spotify import Spotify
-from .Telegram import Telegram
+from .Apple import AppleAPI
+from .Carbon import CarbonAPI
+from .Resso import RessoAPI
+from .Soundcloud import SoundAPI
+from .Spotify import SpotifyAPI
+from .Telegram import TeleAPI
 from .Youtube import YouTube
