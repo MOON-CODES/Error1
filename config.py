3c91f27d536d48fa592b1b2a8defbc702e5ceb87
Moon <101085046+MOON-CODES@users.noreply.github.com>
2024-12-01 16:14:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 600))
 
 # Chat id of a group for logging bot's activities
-LOG_GROUP_ID = getenv("LOG_GROUP_ID", "-1001550054357")
+LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # Get this value from @FallenxBot on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", 6391774843))
